edbee11e89e80893afc1b6d48c3c65b8cbce4cfc
lukvmil <millerluke1364@gmail.com>
2024-11-20 16:44:25 +0800

    moved to hatchling for build, removed setup.py, added unit tests, fixed SlackUser import issue, added dev dependencies

--- src/rid_lib/types/__init__.py
+++ src/rid_lib/types/__init__.py
@@ -2,6 +2,7 @@ from .http_s import HTTP, HTTPS
 from .slack_channel import SlackChannel
 from .slack_message import SlackMessage
 from .slack_workspace import SlackWorkspace
+from .slack_user import SlackUser
 from .discord_channel import DiscordChannel
 from .discord_guild import DiscordGuild
 from .discord_message import DiscordMessage
