681126f47543927f45f7af854eb05efd84ac7ebe
lukvmil <millerluke1364@gmail.com>
2024-12-30 16:10:57 +0800

    update README, removed proposed RID types from __init__ file, bumped version to 3.0.0

--- src/rid_lib/types/__init__.py
+++ src/rid_lib/types/__init__.py
@@ -3,7 +3,3 @@ from .slack_channel import SlackChannel
 from .slack_message import SlackMessage
 from .slack_workspace import SlackWorkspace
 from .slack_user import SlackUser
-from .discord_channel import DiscordChannel
-from .discord_guild import DiscordGuild
-from .discord_message import DiscordMessage
-from .discord_user import DiscordUser
