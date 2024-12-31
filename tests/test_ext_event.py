9df967811da03722af3c2d8e1ed0d7ac1b320ec6
lukvmil <millerluke1364@gmail.com>
2024-12-31 17:16:41 +0800

    fixed event reconstruction bug, added test coverage for events

---
+++ tests/test_ext_event.py
@@ -0,0 +1,16 @@
+from rid_lib import RID
+from rid_lib.ext import Event, EventType, Manifest
+
+def test_event_equivalency():
+    rid = RID.from_string("test:rid")
+    m = Manifest.generate(rid, {})
+
+    e1 = Event(rid, EventType.NEW)
+    e1r = Event.from_json(e1.to_json())
+    assert e1 == e1r
+    assert e1.to_json() == e1r.to_json()
+
+    e2 = Event(rid, EventType.UPDATE, m)
+    e2r = Event.from_json(e2.to_json())
+    assert e2 == e2r
+    assert e2.to_json() == e2r.to_json()
