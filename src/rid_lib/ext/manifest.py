dc03af09e2a074f1fc6ca62cf06bf82226b13a42
lukvmil <millerluke1364@gmail.com>
2025-03-30 16:55:52 +0800

    whoops forgot to clean up -> 3.2.1

--- src/rid_lib/ext/manifest.py
+++ src/rid_lib/ext/manifest.py
@@ -5,25 +5,9 @@ from .utils import sha256_hash_json
 
 
 class Manifest(BaseModel):
-    # created on demand
-    rid: RID       # implicit
-    timestamp: datetime # get current time
-    sha256_hash: str    # requires access to contents
-    
-    # optional?
-    # version: str = "1.0.0"
-    # source_node: RIDField   # must exist within a node
-    # cacheable: bool
-    # schema: ...
-    # event_source: ...
-    # provenance: list[RIDField]
-    # common_fields: dict[str, str] = {
-    #     "text": "text"
-    # }
-    # internal_rids: list[RIDField] = [
-    #     "orn:slack.user"
-    # ]
-    
+    rid: RID
+    timestamp: datetime
+    sha256_hash: str
     
     @classmethod
     def generate(cls, rid: RID, data: dict):
