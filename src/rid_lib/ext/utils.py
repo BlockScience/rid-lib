f5d3e12448b04c4ee40d07a17caea650fd0b3a93
lukvmil <millerluke1364@gmail.com>
2025-04-21 16:12:32 +0800

    updated cache_bundle -> bundle, removed unused json_serialize function (superceded by Pydantic)

--- src/rid_lib/ext/utils.py
+++ src/rid_lib/ext/utils.py
@@ -1,10 +1,7 @@
 import json
 import hashlib
 from base64 import urlsafe_b64encode, urlsafe_b64decode
-from dataclasses import asdict, is_dataclass
-from datetime import datetime
 from pydantic import BaseModel
-from rid_lib import RID
 
 
 def sha256_hash_json(data: dict | BaseModel):
@@ -23,17 +20,3 @@ def b64_encode(string: str):
 def b64_decode(string: str):
     return urlsafe_b64decode(
         (string + "=" * (-len(string) % 4)).encode()).decode()
-
-def json_serialize(obj):
-    if isinstance(obj, RID):
-        return str(obj)
-    elif is_dataclass(obj) and not isinstance(obj, type):
-        return json_serialize(asdict(obj))
-    elif isinstance(obj, datetime):
-        return obj.isoformat()
-    elif isinstance(obj, (list, tuple)):
-        return [json_serialize(item) for item in obj]
-    elif isinstance(obj, dict):
-        return {key: json_serialize(value) for key, value in obj.items()}
-    else:
-        return obj
