fb02ecaccf087366e1eb8e92cb9db211ca28e48a
lukvmil <millerluke1364@gmail.com>
2025-08-05 16:22:33 -0400

    changed koi-net.node field name to `hash` reflecting usage in secure koi-net protocol

--- src/rid_lib/types/koi_net_node.py
+++ src/rid_lib/types/koi_net_node.py
@@ -1,21 +1,16 @@
-from uuid import uuid4
 from rid_lib.core import ORN
 
 
 class KoiNetNode(ORN):
     namespace = "koi-net.node"
     
-    def __init__(self, name, uuid):
-        self.name = name
-        self.uuid = uuid
-        
-    @classmethod
-    def generate(cls, name):
-        return cls(name, uuid4())
+    def __init__(self, name: str, hash: str):
+        self.name: str = name
+        self.hash: str = hash
         
     @property
     def reference(self):
-        return f"{self.name}+{self.uuid}"
+        return f"{self.name}+{self.hash}"
     
     @classmethod
     def from_reference(cls, reference):
@@ -23,4 +18,4 @@ class KoiNetNode(ORN):
         if len(components) == 2:
             return cls(*components)
         else:
-            raise ValueError("KOI-net Node reference must contain two '+'-separated components: '<name>+<uuid>'")
+            raise ValueError("KOI-net Node reference must contain two '+'-separated components: '<name>+<hash>'")
