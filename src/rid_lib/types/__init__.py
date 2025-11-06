8c7eb99f408d09c09ebca00a750a2f870524c1f9
lukvmil <millerluke1364@gmail.com>
2025-11-06 14:54:33 -0500

    updated imports for rid_lib.types

--- src/rid_lib/types/__init__.py
+++ src/rid_lib/types/__init__.py
@@ -6,3 +6,8 @@ from .slack_user import SlackUser
 from .koi_net_node import KoiNetNode
 from .koi_net_edge import KoiNetEdge
 from .normalized_text import NormalizedText
+from .hackmd_note import HackMDNote
+from .discord_guild import DiscordGuild
+from .discord_channel import DiscordChannel
+from .discord_message import DiscordMessage
+from .discord_user import DiscordUser
