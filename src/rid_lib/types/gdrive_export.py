3095667cf5e63bc15ed41a93625baf81c1f9a35d
Joshua Jodesty <joshua.jodesty@gmail.com>
2025-11-14 10:37:44 -0500

    GDrive File, FileAuthor, & Export RIDs

---
+++ src/rid_lib/types/gdrive_export.py
@@ -0,0 +1,28 @@
+from rid_lib.core import ORN
+
+
+class GoogleDriveExport(ORN):
+    namespace = 'google_drive.export'
+    
+    def __init__(self, type: str, subtype: str, id: str):
+        self.type = type
+        self.subtype = subtype
+        self.id = id
+
+    @property
+    def mimeType(self):
+        return f'{self.type}/{self.subtype}'
+    
+    @property
+    def reference(self):
+        return f'{self.mimeType}/{self.id}'
+    
+    @classmethod
+    def from_reference(cls, reference):
+        components = reference.split("/")
+        if len(components) == 3:
+            return cls(*components)
+        else:
+            raise ValueError(
+                "File Export reference must contain 3 '/'-separated components: '<type>/<subtype>/<id>'"
+            )
