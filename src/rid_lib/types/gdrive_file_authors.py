44abb67c30a1014a832637b7d6f2c97123676344
Joshua Jodesty <joshua.jodesty@gmail.com>
2025-11-14 10:37:44 -0500

    GDrive File, FileAuthor, & Export RIDs

---
+++ src/rid_lib/types/gdrive_file_authors.py
@@ -0,0 +1,22 @@
+from rid_lib.core import ORN
+from rid_lib.types.gdrive_file import GoogleDriveFile
+
+class GoogleDriveFileAuthors(ORN):
+    namespace = 'google_drive.file.authors'
+
+    def __init__(self, id: str):
+        self.id = id
+
+    @property
+    def file(self) -> GoogleDriveFile:
+        return GoogleDriveFile(self.id)
+
+    @property
+    def reference(self):
+        return self.id
+            
+    @classmethod
+    def from_reference(cls, id):
+        if len(id) >= 1:
+            return cls(id)
+        return None
