7f0153fa6127bfafc73c037e2f86c961d8979ec2
Joshua Jodesty <joshua.jodesty@gmail.com>
2025-11-19 23:19:34 -0500

    raising exception when class isn't returned from from_reference; including export mimeTypes within GoogleDriveFile and GoogleDriveFileAuthors

--- src/rid_lib/types/gdrive_export.py
+++ src/rid_lib/types/gdrive_export.py
@@ -4,10 +4,10 @@ from rid_lib.core import ORN
 class GoogleDriveExport(ORN):
     namespace = 'google_drive.export'
     
-    def __init__(self, type: str, subtype: str, id: str):
+    def __init__(self, type: str, subtype: str, fileId: str):
         self.type = type
         self.subtype = subtype
-        self.id = id
+        self.fileId = fileId
 
     @property
     def mimeType(self):
@@ -15,14 +15,17 @@ class GoogleDriveExport(ORN):
     
     @property
     def reference(self):
-        return f'{self.mimeType}/{self.id}'
+        return f'{self.mimeType}/{self.fileId}'
     
     @classmethod
     def from_reference(cls, reference):
-        components = reference.split("/")
-        if len(components) == 3:
-            return cls(*components)
+        if type(reference) is str:
+            components = reference.split("/")
+            if len(components) == 3:
+                return cls(*components)
+            else:
+                raise ValueError(
+                    "Google Drive File Export reference must contain 3 '/'-separated components: '<type>/<subtype>/<fileId>'"
+                )
         else:
-            raise ValueError(
-                "File Export reference must contain 3 '/'-separated components: '<type>/<subtype>/<id>'"
-            )
+            raise ValueError("Google Drive File Export reference must be a string")
