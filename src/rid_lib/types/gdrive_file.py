f391f2a0014f1fbba1d9b452c45b2c7621a960f5
Joshua Jodesty <joshua.jodesty@gmail.com>
2025-11-19 23:19:34 -0500

    raising exception when class isn't returned from from_reference; including export mimeTypes within GoogleDriveFile and GoogleDriveFileAuthors

--- src/rid_lib/types/gdrive_file.py
+++ src/rid_lib/types/gdrive_file.py
@@ -1,9 +1,26 @@
 from rid_lib.core import ORN
 from rid_lib.types.gdrive_export import GoogleDriveExport
 
+
+document_mimeType = 'application/vnd.google-apps.document'
+sheets_mimeType = 'application/vnd.google-apps.spreadsheet'
+slides_mimeType = 'application/vnd.google-apps.presentation'
+
+export_mimeType_map = {
+    document_mimeType: {'text': {'plain': 'text/plain', 'markdown': 'text/markdown'}},
+    sheets_mimeType: {'text': {'csv': 'text/csv', 'tab-separated-values': 'text/tab-separated-values'}},
+    slides_mimeType: {'text': {'plain': 'text/plain'}}
+}
+export_mimeTypes = {
+    document_mimeType: export_mimeType_map[document_mimeType]['text']['plain'], # 'text/plain'
+    sheets_mimeType: export_mimeType_map[sheets_mimeType]['text']['csv'], # 'text/csv'
+    slides_mimeType: export_mimeType_map[slides_mimeType]['text']['plain'], # 'text/plain'
+}
+
 class GoogleDriveFile(ORN):
     namespace = 'google_drive.file'
     mimeType = 'application/vnd.google-apps.file'
+    export_mimeTypes = export_mimeTypes
 
     def __init__(self, id: str):
         self.id = id
@@ -17,7 +34,7 @@ class GoogleDriveFile(ORN):
         return GoogleDriveExport(
             type=type, 
             subtype=subtype, 
-            id=self.id
+            fileId=self.id
         )
 
     @property
@@ -25,7 +42,11 @@ class GoogleDriveFile(ORN):
         return self.id
 
     @classmethod
-    def from_reference(cls, id):
-        if len(id) >= 1:
-            return cls(id)
-        return None
+    def from_reference(cls, id: str):
+        if type(id) is str:
+            if len(id) >= 1:
+                return cls(id)
+            else:
+                raise ValueError("Google Drive File reference must not be an empty string")
+        else:
+            raise ValueError("Google Drive File reference must be a string")
--- src/rid_lib/types/gdrive_file_authors.py
+++ src/rid_lib/types/gdrive_file_authors.py
@@ -1,22 +1,35 @@
+from pydantic import EmailStr, ValidationError
+
 from rid_lib.core import ORN
 from rid_lib.types.gdrive_file import GoogleDriveFile
 
+
 class GoogleDriveFileAuthors(ORN):
     namespace = 'google_drive.file.authors'
+    export_mimeTypes = GoogleDriveFile.export_mimeTypes
 
-    def __init__(self, id: str):
-        self.id = id
+    def __init__(self, fileId: str):
+        self.fileId = fileId
 
     @property
     def file(self) -> GoogleDriveFile:
-        return GoogleDriveFile(self.id)
+        return GoogleDriveFile(self.fileId)
 
     @property
     def reference(self):
-        return self.id
+        return self.fileId
             
     @classmethod
-    def from_reference(cls, id):
-        if len(id) >= 1:
-            return cls(id)
-        return None
+    def from_reference(cls, fileId: str):
+        if type(fileId) is str:
+            if len(fileId) >= 1:
+                try:
+                    # Validate email using Pydantic's EmailStr
+                    EmailStr._validate(fileId)
+                    return cls(fileId)
+                except ValidationError:
+                    raise ValueError(f"Google Drive File Authors reference must be a valid email address: {fileId}")
+            else:
+                raise ValueError("Google Drive File Authors reference must not be an empty string")
+        else:
+            raise ValueError("Google Drive File Authors reference must be a string")
