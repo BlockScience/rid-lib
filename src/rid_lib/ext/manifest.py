27da2256a9b3a524f6139dfaff6e5ae37250fb9c
lukvmil <millerluke1364@gmail.com>
2026-05-08 15:07:33 -0400

    v3.3.0b1 - allows extra fields in manifests, and adds an `@context` when using the generate method

--- src/rid_lib/ext/manifest.py
+++ src/rid_lib/ext/manifest.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from rid_lib.core import RID
 from .utils import sha256_hash_json
 
@@ -13,11 +13,14 @@ class Manifest(BaseModel):
     timestamp: datetime
     sha256_hash: str
     
+    model_config = ConfigDict(extra="allow")
+    
     @classmethod
     def generate(cls, rid: RID, data: dict) -> "Manifest":
         """Generates a Manifest using the current time and hashing the provided data."""
         return cls(
             rid=rid,
             timestamp=datetime.now(timezone.utc),
-            sha256_hash=sha256_hash_json(data)
+            sha256_hash=sha256_hash_json(data),
+            **{"@context": "orn:koi-net.context:manifest"}
         )
